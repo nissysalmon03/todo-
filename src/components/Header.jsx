6da6671bdb5f8bf5fc18e694979a3f9600b3c167
nissysalmon03 <nissysalmon03@github.com>
2026-04-24 01:04:10 +0530

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>My To-Do List</h1>
+      <p>Stay organized and boost your productivity</p>
+    </header>
+  );
+}
+
+export default Header;
